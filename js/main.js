e436266d14e7f07997aa3b1aa0a93b647e12083c
Nick Hughes <nkhughes@outlook.com>
2018-02-27 20:04:33 -0500

    added google map on contact page and fixed scroll top on index.html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
+$(document ).ready(function(){
 	
 	$('.dropdown').on('click', function(){
 		$('.drpdwncontent').toggleClass('style');
 		$('body').toggleClass('nav-bar-open');
 	});
 	
-	$('#top').on('click', function(){
-		$(window).scrollTop(0);
-	});
+	$('#top').click(function(){
+    $('html,body').animate({scrollTop: 0}, 500);
+});
+	
+  
 }); // closes my document.ready
