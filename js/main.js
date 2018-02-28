a8d4a01e2920ac359702468d7cb2c3a2b7a86790
Nick Hughes <nkhughes@outlook.com>
2018-02-27 21:42:13 -0500

    resolved gmaps issue, and created a form

--- js/main.js
+++ js/main.js
@@ -9,5 +9,18 @@ $(document ).ready(function(){
     $('html,body').animate({scrollTop: 0}, 500);
 });
 	
+/*
+	$('#submit-button').on('click', function(){
+			var firstName = $('#fname').val()
+			var lastName = $('#lname').val()
+			var emailAdd = $('#email').val()
+			var commentBox = $('#comments').val()
+			console.log(firstName + ' ' + lastName + ' ' + emailAdd + ' ' + commentBox)
+			window.location.href = 'mailto: nhughes118@gmail.com'
+		});
+*/
+
+
+
   
 }); // closes my document.ready
