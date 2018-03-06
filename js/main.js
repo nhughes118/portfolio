7fc4c5bdb3a37d02fbf94ce678e4b9fffe30faa9
Nick Hughes <nkhughes@outlook.com>
2018-03-06 13:57:23 -0500

    fixed some portfolio dropdown links

--- js/main.js
+++ js/main.js
@@ -12,5 +12,33 @@ $(document).ready(function(){
 	$(function(){
 		$('#pic-js').animate({'bottom': '0px'}, 1000);
 	});
+	
+	$('#site-1').click(function(){
+		$('html,body').animate({
+			'scrollTop': $('#scroll-1').offset().top-100
+		}, 500);
+	});	
+	
+	$('#site-2').click(function(){
+		$('html,body').animate({
+			'scrollTop': $('#scroll-2').offset().top-100
+		}, 500);
+	});	
+	
+	$('#site-3').click(function(){
+		$('html,body').animate({
+			'scrollTop': $('#scroll-3').offset().top-100
+		}, 500);
+	});	
+	
+	$('#site-4').click(function(){
+		$('html,body').animate({
+			'scrollTop': $('#scroll-4').offset().top-100
+		}, 500);
+	});
+	
+	$('.site-1-all').click(function(){
+		window.location.href = "portfolio.html";
+	});
   
 }); // closes my document.ready
