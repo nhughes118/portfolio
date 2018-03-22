e2dd9d1cf4962dba2b282abfdaf38ab4f70d719d
Nick Hughes <nkhughes@outlook.com>
2018-03-22 15:33:44 -0400

    added scroll top button and changed portfolio drop down to drop down on page itself

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	
 	$('#top').click(function(){
     $('html,body').animate({scrollTop: 0}, 500);
-});
+	});
 
 	$(function(){
 		$('#pic-js').animate({'bottom': '0px'}, 1000);
@@ -71,6 +71,22 @@ $(document).ready(function(){
 		$('html,body').animate({
 			'scrollTop': $('#scroll-4').offset().top - offset
 		});
+	});
+	
+// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+	
+	function scrollFunction() {
+		if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+		    document.getElementById("myBtn").style.display = "block";
+		} else {
+		    document.getElementById("myBtn").style.display = "none";
+		}
+	}
+	
+// When the user clicks on the button, scroll to the top of the document
+	$('#myBtn').click(function(){
+		$('html,body').animate({scrollTop: 0}, 500);
 	});
   
 }); // closes my document.ready
